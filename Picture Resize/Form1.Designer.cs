efb6f54ab26a5e4b1c870cf0528d8d6907d2f38e
sahincabbar <36867588+sahincabbar@users.noreply.github.com>
2018-08-14 12:26:51 +0400

    Testing

---
+++ Picture Resize/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Picture_Resize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Location = new System.Drawing.Point(621, 35);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PictureBox1.TabIndex = 1;
+            this.PictureBox1.TabStop = false;
+            // 
+            // Button1
+            // 
+            this.Button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Button1.Location = new System.Drawing.Point(497, 601);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(112, 54);
+            this.Button1.TabIndex = 2;
+            this.Button1.Text = "Choose file";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(922, 601);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 457);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(319, 457);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 462);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Width :";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(485, 452);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 33);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(954, 458);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 462);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Height :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(821, 463);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Original Width :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1129, 460);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Original Height :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1275, 457);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(485, 540);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 33);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Apply";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(239, 550);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 17);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Height :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 550);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Width :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(319, 545);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.Validating += new System.ComponentModel.CancelEventHandler(this.textBox5_Validating);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(88, 545);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.Validating += new System.ComponentModel.CancelEventHandler(this.textBox6_Validating);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(239, 400);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 17);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Resize manually";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(239, 511);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Resize automatically";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 680);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.PictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Picture Resize/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Resize
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+        SaveFileDialog sfd = new SaveFileDialog();
+        public string Filename;
+        public string FileAdress;
+        public double _width;
+        public double _height;
+        public double dividedsize;
+        public void Button1_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "JPG|*.jpg";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string filename = ofd.FileName;
+                FileAdress = filename;
+                var filePath = ofd;
+                Image image = new Bitmap(filename, true);
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                PictureBox1.Image = image;
+                label1.Text = filename;
+                textBox3.Text = Convert.ToString(image.Width + "px");
+                textBox4.Text = Convert.ToString(image.Height + "px");
+                _width = (float)image.Width;
+                _height = (float)image.Height;
+                dividedsize = _width / _height;
+                textBox5.Text = "";
+                textBox6.Text = "";
+                Filename = ofd.SafeFileName;
+                Bitmap imgImage = new Bitmap(PictureBox1.Image);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _width = Convert.ToInt32(textBox1.Text);
+                _height = Convert.ToInt32(textBox2.Text);
+                PictureBox1.Width = (int)_width;
+                PictureBox1.Height = (int)_height;
+                textBox3.Text = Convert.ToString(PictureBox1.Width + "px");
+                textBox4.Text = Convert.ToString(PictureBox1.Height + "px");
+                var image = Image.FromFile(ofd.FileName);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter in the right format (Enter numbers)");
+            }
+
+         
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string manw = textBox6.Text;
+            double bb = Convert.ToDouble(manw);
+            string manh = textBox5.Text;
+            double cc = Convert.ToDouble(manh);
+            double manualw = (double)bb;
+            double manualh = (double)cc;
+            _width = manualw;
+            _height =manualh;
+            PictureBox1.Width = (int)_width;
+            PictureBox1.Height = (int)_height;
+            textBox5.Text = Convert.ToString(PictureBox1.Width + "px");
+            textBox6.Text = Convert.ToString(PictureBox1.Height + "px");
+            //try
+            //{
+               
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("Please enter in the right format (Enter numbers)");
+            //}
+        }
+
+        private void textBox6_Validating(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                var d = Convert.ToInt32(textBox6.Text);
+                double x = (double)d;
+                textBox5.Text = Convert.ToString((x / dividedsize));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter in the right format (Enter numbers)");
+            }
+        }
+
+        private void textBox5_Validating(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                var g = Convert.ToInt32(textBox5.Text);
+                double y = (double)g;
+                textBox6.Text = Convert.ToString((dividedsize * y));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter in the right format (Enter numbers)");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Size size = new Size((int)_width, (int)_height);
+            Image resultImage = new Bitmap(PictureBox1.Image, size);
+            resultImage.Save(Filename);
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.InitialDirectory = @"C:\Users\acer\Desktop\SubDir";
+            saveFile.Filter = "JPG | *.jpg";
+            saveFile.Title = "Save an image";
+            saveFile.AddExtension = true;
+            saveFile.DefaultExt = "bmp";
+            saveFile.FilterIndex = 2;
+            saveFile.RestoreDirectory = true;
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string fName = saveFile.FileName;
+                resultImage.Save(fName, ImageFormat.Bmp);
+            };
+        }
+    }
+}
+
+
+
+
